5b33f2f418237969e221336bab34d7a9cef7f5f8
igokydear <igokydear19@gmail.com>
2022-09-22 20:49:02 +0700

    menambah media query

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function () {
   const stickyNavbar = this.document.getElementById("navbar");
   stickyNavbar.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector("#nav-list");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+});
